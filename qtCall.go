9a6ae72fb998a4dee8dca7748d4bf4a1a8cdb1e7
qtgolang <564998512@qq.com>
2021-10-19 21:01:56 +0800

    .

--- qtCall.go
+++ qtCall.go
@@ -13,8 +13,8 @@ import (
 // Call
 //
 // Call 一个内存地址 只能接收 uintptr int int8 int16 int32 int64  string []byte bool 类型
-// 返回-1 表示失败 返回其他并表示执行成功  返回 Call 的目标函数的返回值返回数据的指针
-func Call(address int, arg ...interface{}) uintptr {
+// 返回-1 表示失败 返回其他并表示执行成功  返回 Call 的目标函数的返回值返回数据的指针  uintptr(ret)
+func Call(address int, arg ...interface{}) int {
 
 	var args []uintptr
 	var Frees []*C.char
@@ -54,9 +54,9 @@ func Call(address int, arg ...interface{}) uintptr {
 	for index := 0; index < (18 - slen); index++ {
 		args = append(args, uintptr(0))
 	}
-	r1, _, _ := syscall.Syscall18(uintptr(address), uintptr(slen), args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17])
+	ret, _, _ := syscall.Syscall18(uintptr(address), uintptr(slen), args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17])
 	for index := 0; index < len(Frees); index++ {
 		C.free(unsafe.Pointer(Frees[index]))
 	}
-	return r1
+	return int(ret)
 }
